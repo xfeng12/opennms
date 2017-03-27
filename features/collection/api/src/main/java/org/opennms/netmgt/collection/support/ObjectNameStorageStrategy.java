380c65a9568f1b0c79f8f3f2c75bee8d5457b321
Ronald Roskens <ronald.roskens@gmail.com>
2017-03-27 11:21:26 -0500

    Split the Jexl part out into its own file so it can be used independently of ObjectName.

--- features/collection/api/src/main/java/org/opennms/netmgt/collection/support/ObjectNameStorageStrategy.java
+++ features/collection/api/src/main/java/org/opennms/netmgt/collection/support/ObjectNameStorageStrategy.java
@@ -28,57 +28,26 @@
 
 package org.opennms.netmgt.collection.support;
 
-import java.util.List;
 import javax.management.ObjectName;
 
 import org.apache.commons.jexl2.JexlContext;
-import org.apache.commons.jexl2.JexlEngine;
-import org.apache.commons.jexl2.JexlException;
-import org.apache.commons.jexl2.MapContext;
-import org.apache.commons.jexl2.ReadonlyContext;
-import org.apache.commons.jexl2.UnifiedJEXL;
 import org.opennms.netmgt.collection.api.CollectionResource;
-import org.opennms.netmgt.config.datacollection.Parameter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ObjectNameStorageStrategy extends IndexStorageStrategy {
+public class ObjectNameStorageStrategy extends JexlIndexStorageStrategy {
 
     private static final Logger LOG = LoggerFactory.getLogger(ObjectNameStorageStrategy.class);
-    private static final int DEFAULT_JEXLENGINE_CACHESIZE = 512;
     private static final String QUOTE = "\"";
-
-    private static final String PARAM_INDEX_FORMAT = "index-format";
-    private static final String PARAM_CLEAN_OUTPUT = "clean-output";
-    private String m_indexFormat;
-    private Boolean m_cleanOutput = false;
-
-    private static final JexlEngine JEXL_ENGINE;
-    private static final UnifiedJEXL EL;
-
-    static {
-        final int cacheSize = Integer.getInteger("org.opennms.netmgt.collection.support.ObjectNameStorageStrategy.cacheSize", DEFAULT_JEXLENGINE_CACHESIZE);
-        JEXL_ENGINE = new JexlEngine();
-        JEXL_ENGINE.setCache(cacheSize);
-        JEXL_ENGINE.setLenient(false);
-        JEXL_ENGINE.setStrict(true);
-
-        EL = new UnifiedJEXL(JEXL_ENGINE);
-    }
 
     public ObjectNameStorageStrategy() {
         super();
     }
 
-    /** {@inheritDoc} */
     @Override
-    public String getResourceNameFromIndex(CollectionResource resource) {
-        String resourceName = null;
+    public void updateContext(JexlContext context, CollectionResource resource) {
         try {
             ObjectName oname = new ObjectName(resource.getInstance());
-            UnifiedJEXL.Expression expr = EL.parse( m_indexFormat );
-
-            JexlContext context = new MapContext();
             context.set("ObjectName", oname);
             context.set("domain", oname.getDomain() == null ? "" : oname.getDomain());
             oname.getKeyPropertyList().entrySet().forEach((entry) -> {
@@ -89,50 +58,8 @@ public class ObjectNameStorageStrategy extends IndexStorageStrategy {
                     context.set(entry.getKey(), entry.getValue());
                 }
             });
-            resourceName = (String) expr.evaluate(new ReadonlyContext(context));
-        } catch (JexlException e) {
-            LOG.error("getResourceNameFromIndex(): error evaluating index-format [{}] as a Jexl Expression", m_indexFormat, e);
         } catch (javax.management.MalformedObjectNameException e) {
             LOG.debug("getResourceNameFromIndex(): malformed object name: {}", resource.getInstance(), e);
-        } finally {
-            if (resourceName == null) {
-                resourceName = resource.getInstance();
-                resourceName = resourceName.replaceAll("\\s+", "_").replaceAll(":", "_").replaceAll("\\\\", "_").replaceAll("[\\[\\]]", "_").replaceAll("[|/]", "_").replaceAll("=", "").replaceAll("[_]+$", "").replaceAll("___", "_");
-            }
-        }
-        if (m_cleanOutput && resourceName != null) {
-            resourceName = resourceName.replaceAll("\\s+", "_").replaceAll(":", "_").replaceAll("\\\\", "_").replaceAll("[\\[\\]]", "_").replaceAll("[|/]", "_").replaceAll("=", "").replaceAll("[_]+$", "").replaceAll("___", "_");
-        }
-
-        LOG.debug("getResourceNameFromIndex(): {}", resourceName);
-        return resourceName;
-    }
-
-    /** {@inheritDoc} */
-    @Override
-    public void setParameters(List<Parameter> parameterCollection) throws IllegalArgumentException {
-        if (parameterCollection == null) {
-            final String msg ="Got a null parameter list, but need one containing a '" + PARAM_INDEX_FORMAT + "' parameter.";
-            LOG.error(msg);
-            throw new IllegalArgumentException(msg);
-        }
-        parameterCollection.forEach((param) -> {
-            if (null == param.getKey()) {
-                LOG.warn("Encountered unsupported parameter key=\"{}\". Can accept: {}, {}", param.getKey(), PARAM_INDEX_FORMAT, PARAM_CLEAN_OUTPUT);
-            } else switch (param.getKey()) {
-                case PARAM_INDEX_FORMAT:
-                    m_indexFormat = param.getValue().trim();
-                    break;
-                case PARAM_CLEAN_OUTPUT:
-                    m_cleanOutput = Boolean.parseBoolean(param.getValue().trim());
-                    break;
-                default:
-                    LOG.warn("Encountered unsupported parameter key=\"{}\". Can accept: {}, {}", param.getKey(), PARAM_INDEX_FORMAT, PARAM_CLEAN_OUTPUT);
-                    break;
-            }
-        });
-        if (m_indexFormat == null) {
-            throw new IllegalArgumentException("Missing index-format expression");
         }
     }
 }
